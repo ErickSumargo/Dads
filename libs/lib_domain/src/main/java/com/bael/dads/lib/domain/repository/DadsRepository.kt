bb34e68b9ee3323b7a15b59ecd7d8ac86c64e2e7
ErickSumargo <ericksumargo@outlook.com>
2021-01-01 16:04:44 +0700

    Migrate Response Model to lib_data (#9)

--- libs/lib_domain/src/main/java/com/bael/dads/lib/domain/repository/DadsRepository.kt
+++ libs/lib_domain/src/main/java/com/bael/dads/lib/domain/repository/DadsRepository.kt
@@ -1,6 +1,6 @@
 package com.bael.dads.lib.domain.repository
 
-import com.bael.dads.lib.domain.model.Response
+import com.bael.dads.lib.data.response.Response
 import kotlinx.coroutines.flow.Flow
 
 /**
