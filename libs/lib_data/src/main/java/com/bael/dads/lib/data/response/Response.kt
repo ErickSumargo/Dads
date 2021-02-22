435e329e1430a23ce8b48b7b978a218bc61de9e0
Erick Sumargo <ericksumargo@outlook.com>
2021-02-22 11:33:41 +0700

    normalized

--- libs/lib_data/src/main/java/com/bael/dads/lib/data/response/Response.kt
+++ libs/lib_data/src/main/java/com/bael/dads/lib/data/response/Response.kt
@@ -1,14 +1,18 @@
 package com.bael.dads.lib.data.response
 
+import java.io.Serializable
+
 /**
  * Created by ErickSumargo on 01/01/21.
  */
 
-sealed class Response {
+sealed class Response<out T> : Serializable {
 
-    object Loading : Response()
+    object Loading : Response<Nothing>()
 
-    data class Success<T>(val data: T) : Response()
+    data class Error(val error: Exception) : Response<Nothing>()
 
-    data class Error(val error: Exception) : Response()
+    object Empty : Response<Nothing>()
+
+    data class Success<T>(val data: T) : Response<T>()
 }
