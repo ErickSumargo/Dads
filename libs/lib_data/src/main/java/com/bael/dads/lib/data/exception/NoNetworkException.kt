12699edb83f09956aa53e4df26ecd31b2171a72e
ErickSumargo <ericksumargo@outlook.com>
2021-02-22 11:34:59 +0700

    :lib_data Normalization (#19)

--- libs/lib_data/src/main/java/com/bael/dads/lib/data/exception/NoNetworkException.kt
+++ libs/lib_data/src/main/java/com/bael/dads/lib/data/exception/NoNetworkException.kt
@@ -6,7 +6,4 @@ import java.io.IOException
  * Created by ErickSumargo on 01/01/21.
  */
 
-class NoNetworkException : IOException() {
-    override val message: String
-        get() = "No Network Access"
-}
+class NoNetworkException : IOException()
