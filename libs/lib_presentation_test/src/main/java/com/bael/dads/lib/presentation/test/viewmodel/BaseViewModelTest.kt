a7aa99aef9f1d5c1e38717a5b0af49d7a5ed363d
ErickSumargo <ericksumargo@outlook.com>
2021-03-07 20:33:39 +0700

    :lib_presentation_test (#48)

---
+++ libs/lib_presentation_test/src/main/java/com/bael/dads/lib/presentation/test/viewmodel/BaseViewModelTest.kt
@@ -0,0 +1,64 @@
+package com.bael.dads.lib.presentation.test.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import com.bael.dads.lib.threading.Thread
+import dagger.hilt.android.testing.HiltAndroidRule
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+
+/**
+ * Created by ErickSumargo on 01/01/21.
+ */
+
+abstract class BaseViewModelTest {
+    @get:Rule
+    internal val hiltRule = HiltAndroidRule(this)
+
+    protected lateinit var savedStateHandle: SavedStateHandle
+
+    protected lateinit var testDispatcher: TestCoroutineDispatcher
+
+    protected val thread: Thread = object : Thread {
+        override val main: CoroutineDispatcher
+            get() = testDispatcher
+
+        override val default: CoroutineDispatcher
+            get() = testDispatcher
+
+        override val io: CoroutineDispatcher
+            get() = testDispatcher
+    }
+
+    @Before
+    internal fun beforeEachTest() {
+        hiltRule.inject()
+
+        savedStateHandle = SavedStateHandle()
+        testDispatcher = TestCoroutineDispatcher()
+
+        Dispatchers.setMain(testDispatcher)
+
+        setupBeforeEachTest()
+    }
+
+    abstract fun setupBeforeEachTest()
+
+    protected inline fun testSuspend(crossinline block: suspend () -> Unit) {
+        testDispatcher.runBlockingTest {
+            block()
+        }
+    }
+
+    @After
+    internal fun afterEachTest() {
+        clearAfterEachTest()
+    }
+
+    abstract fun clearAfterEachTest()
+}
