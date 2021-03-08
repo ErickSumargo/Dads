8bd27bdb2604b55b1721ae221e36f0edb099b707
ErickSumargo <ericksumargo@outlook.com>
2021-03-08 08:17:38 +0700

    :feature_home Instrumentation Test Sampling (#49)

--- libs/lib_api_test/src/main/java/com/bael/dads/lib/api/test/di/module/service/ServiceTestModule.kt
+++ libs/lib_api_test/src/main/java/com/bael/dads/lib/api/test/di/module/service/ServiceTestModule.kt
@@ -1,11 +1,15 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.bael.dads.lib.api.test.di.module.service
 
 import com.bael.dads.lib.api.di.module.service.ServiceModule
 import com.bael.dads.lib.api.response.DadJokesResponse
-import com.bael.dads.lib.api.test.service.DadsTestableService
+import com.bael.dads.lib.api.service.DadsService
+import com.bael.dads.lib.api.test.service.TestableService
 import com.bael.dads.lib.api.test.service.fake.DadsFakeService
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
 import javax.inject.Singleton
@@ -19,9 +23,19 @@ import javax.inject.Singleton
     components = [SingletonComponent::class],
     replaces = [ServiceModule::class]
 )
-internal interface ServiceModule {
+internal interface ServiceTestModule {
 
     @Binds
     @Singleton
-    fun bindDadsService(service: DadsFakeService): DadsTestableService<DadJokesResponse>
+    fun bindDadsService(service: DadsFakeService): DadsService
+
+    companion object {
+
+        @Provides
+        fun provideDadsTestableService(
+            service: DadsService
+        ): TestableService<DadJokesResponse> {
+            return service as TestableService<DadJokesResponse>
+        }
+    }
 }
